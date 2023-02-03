1ccfd36ace1d822bebe1e8d9780a0566a743f13b
A.L <xenide@users.noreply.github.com>
2023-02-03 20:45:59 +0800

    feat: add UI components for selecting token

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,62 +1,73 @@
 import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+import {Heading, VStack, List, ListIcon, ListItem, Divider, Container, Box} from '@chakra-ui/react';
+import {Header} from "../../modules";
+import {Badge, Select} from "@web3uikit/core";
 
 const Home = () => {
   return (
     <VStack w={'full'}>
       <Heading size="md" marginBottom={6}>
-        Ethereum Boilerplate
+        Sample Swap
       </Heading>
-      <List spacing={3}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Moralis authentication
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display Transactions
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Multichain Support
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Using Moralis from client-side
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Adding explorer links to balances, transactions ...
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Better responsive design
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Rainbowkit integration
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          ... and more
-        </ListItem>
-      </List>
+      {/*<List spacing={3}>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Moralis authentication*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Display Transactions*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Display ERC20 transfers*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Display ERC20 balances*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Display NFT balances*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Display NFT transfers*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Multichain Support*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
+      {/*    Using Moralis from client-side*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
+      {/*    Adding explorer links to balances, transactions ...*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
+      {/*    Better responsive design*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
+      {/*    Rainbowkit integration*/}
+      {/*  </ListItem>*/}
+      {/*  <ListItem>*/}
+      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
+      {/*    ... and more*/}
+      {/*  </ListItem>*/}
+      {/*</List>*/}
+
+      {/*<Divider></Divider>*/}
+
+      <Container>
+        <Badge text={'From Token'}/>
+          <Select id={'from'} />
+        <Badge text={'To Token'}/>
+          <Select id={'to'} />
+      </Container>
     </VStack>
   );
 };
