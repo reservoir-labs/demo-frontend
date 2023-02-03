b4d348cf0c2229d049542e2d6022a3c8d629a7af
A.L <xenide@users.noreply.github.com>
2023-02-03 22:27:25 +0800

    chore: upgrade sdk version to 0.0.2

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -1,7 +1,7 @@
 import { ISubNav } from '../SubNav/SubNav';
 
 const NAV_LINKS: ISubNav[] = [
-  { label: 'Home', href: '/' },
+  { label: 'Swap', href: '/' },
   {
     label: 'Add liq',
     href: '/transactions',
