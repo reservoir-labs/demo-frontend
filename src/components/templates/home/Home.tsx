b628cd82688a4bd51d5495a3972047731eb6b0df
A.L <xenide@users.noreply.github.com>
2023-02-03 21:57:01 +0800

    fix: remove unused code

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -20,59 +20,6 @@ const Home = () => {
       <Heading size="md" marginBottom={6}>
         Sample Swap
       </Heading>
-      {/*<List spacing={3}>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Moralis authentication*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Display Transactions*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Display ERC20 transfers*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Display ERC20 balances*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Display NFT balances*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Display NFT transfers*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Multichain Support*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={CheckCircleIcon} color="green.500" />*/}
-      {/*    Using Moralis from client-side*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
-      {/*    Adding explorer links to balances, transactions ...*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
-      {/*    Better responsive design*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
-      {/*    Rainbowkit integration*/}
-      {/*  </ListItem>*/}
-      {/*  <ListItem>*/}
-      {/*    <ListIcon as={SettingsIcon} color="green.500" />*/}
-      {/*    ... and more*/}
-      {/*  </ListItem>*/}
-      {/*</List>*/}
-
-      {/*<Divider></Divider>*/}
-
       <Container>
         <Badge text={'From Token'}/>
           <Select id={'from'} options={[{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]}/>
