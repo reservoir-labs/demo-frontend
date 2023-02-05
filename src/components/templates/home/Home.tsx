521d405310d7038b0ff432caa0c6880e106196d1
A.L <xenide@users.noreply.github.com>
2023-02-05 13:27:00 +0800

    feat: add onChange for select option change

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -2,21 +2,25 @@ import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
 import {
     Heading,
     VStack,
-    List,
-    ListIcon,
-    ListItem,
-    Divider,
     Container,
-    Box,
     NumberInputField,
     NumberInput
 } from '@chakra-ui/react';
 import {Badge, OptionProps, Select} from "@web3uikit/core";
 
-
 const tokenSelectOptions: OptionProps[] = [{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]
 
 const Home = () => {
+
+  const fromTokenChanged = (option: OptionProps) => {
+    console.log("haha")
+    console.log(option)
+  }
+
+  const toTokenChanged = (option: OptionProps) => {
+
+  }
+
   return (
     <VStack w={'full'}>
       <Heading size="md" marginBottom={6}>
@@ -24,13 +28,13 @@ const Home = () => {
       </Heading>
       <Container>
         <Badge text={'From Token'}/>
-          <Select label='select a token' id={'from'} options={tokenSelectOptions} />
+          <Select label='select a token' id={'from'} options={tokenSelectOptions} onChange={fromTokenChanged}/>
           <NumberInput min={0} max={1000000}>
               <NumberInputField>
               </NumberInputField>
           </NumberInput>
         <Badge text={'To Token'}/>
-          <Select label='select a token' id={'to'} options={tokenSelectOptions}/>
+          <Select label='select a token' id={'to'} options={tokenSelectOptions} onChange={toTokenChanged}/>
           <NumberInput min={0} max={1000000}>
               <NumberInputField>
               </NumberInputField>
