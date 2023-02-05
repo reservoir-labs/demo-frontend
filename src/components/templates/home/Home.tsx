aec46b2a719af5bb057e979df402e2030681000d
A.L <xenide@users.noreply.github.com>
2023-02-05 15:38:04 +0800

    wip to get fetch relevant pairs going

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -8,32 +8,40 @@ import {
 import {Badge, OptionProps, Select} from "@web3uikit/core";
 import {Fetcher, Trade} from '@reservoir-labs/sdk'
 import {Token} from "@reservoir-labs/sdk-core";
+import {BaseProvider, WebSocketProvider} from "@ethersproject/providers";
 
 const tokenSelectOptions: OptionProps[] = [{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]
 
 const CHAINID = 43114
 
 const TOKEN_ADDRESS = {
-    'USDC': '0x0000000000000000000000000000000000000000',
-    'USDT': '0x0000000000000000000000000000000000000000',
-    'WAVAX': '0x0000000000000000000000000000000000000000'
+    43114: {
+        'USDC': '0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5',
+        'USDT': '0xfbC22278A96299D91d41C453234d97b4F5Eb9B2d',
+        'WAVAX': '0x46b142DD1E924FAb83eCc3c08e4D46E82f005e0E'
+    }
 }
 const Home = () => {
   let fromToken: OptionProps | null
   let toToken: OptionProps | null
 
+  let provider: BaseProvider = new WebSocketProvider('ws://127.0.0.1:8545')
+
   const fromTokenChanged = (option: OptionProps) => {
       fromToken = option
   }
 
-  const toTokenChanged = (option: OptionProps) => {
+  const toTokenChanged = async (option: OptionProps) => {
     toToken = option
 
-    const relevantPairs = Fetcher.fetchRelevantPairs(
+    const relevantPairs = await Fetcher.fetchRelevantPairs(
         CHAINID,
-        new Token(CHAINID, TOKEN_ADDRESS[fromToken.id], 18),
-        new Token(CHAINID, TOKEN_ADDRESS[toToken.id], 18),
+        new Token(CHAINID, TOKEN_ADDRESS[CHAINID][fromToken.id], 18),
+        new Token(CHAINID, TOKEN_ADDRESS[CHAINID][toToken.id], 18),
+        provider
     );
+
+    console.log("relevantPairs", relevantPairs)
 
     // call Trade.bestTradeExactIn()
   }
