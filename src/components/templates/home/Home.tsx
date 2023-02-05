c2a7b96184038414fe13d3e1467cbce149eb2af5
A.L <xenide@users.noreply.github.com>
2023-02-05 13:12:34 +0800

    refactor: avoid duplicate for options

--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -12,7 +12,10 @@ import {
     NumberInput
 } from '@chakra-ui/react';
 import {Header} from "../../modules";
-import {Badge, Select} from "@web3uikit/core";
+import {Badge, OptionProps, Select} from "@web3uikit/core";
+
+
+const tokenSelectOptions: OptionProps[] = [{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]
 
 const Home = () => {
   return (
@@ -22,13 +25,13 @@ const Home = () => {
       </Heading>
       <Container>
         <Badge text={'From Token'}/>
-          <Select id={'from'} options={[{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]}/>
+          <Select id={'from'} options={tokenSelectOptions} />
           <NumberInput min={0} max={1000000}>
               <NumberInputField>
               </NumberInputField>
           </NumberInput>
         <Badge text={'To Token'}/>
-          <Select id={'to'} options={[{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]}/>
+          <Select id={'to'} options={tokenSelectOptions}/>
           <NumberInput min={0} max={1000000}>
               <NumberInputField>
               </NumberInputField>
