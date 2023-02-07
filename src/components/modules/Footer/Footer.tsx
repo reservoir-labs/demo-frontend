6d88a0c99bd237b57c5bcccfac2c48aab532724b
A.L <xenide@users.noreply.github.com>
2023-02-07 12:07:22 +0800

    feat: sample swap with SDK
    * feat: add UI components for selecting token
    
    * fix: add options
    
    * feat: add NumberInput
    
    * fix: remove unused code
    
    * chore: disable some pages and rename tabs
    
    * chore: upgrade sdk version to 0.0.2
    
    * refactor: avoid duplicate for options
    
    * fix: add label for Select
    
    * feat: add onChange for select option change
    
    * wip
    
    * wip to get fetch relevant pairs going
    
    * wip: attempting to call bestTradeExactIn
    
    * wip: attempting to call bestTradeExactIn
    
    * lib: update sdk version
    
    * fix: bestTradeExactIn finally not crashing
    
    * feat: output amount is displayed in the box successfully
    
    * feat: using useControllableState doesn't seem to be working so well
    
    * feat: responsive design seems to be done
    
    * fix: check route length before accessing

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -10,25 +10,25 @@ const links = {
 const Footer = () => {
   return (
     <Box textAlign={'center'} w="full" p={6}>
-      <Text>
-        ⭐️ Please star this{' '}
-        <Link href={links.github} isExternal alignItems={'center'}>
-          boilerplate <ExternalLinkIcon />
-        </Link>
-        , every star makes us very happy!
-      </Text>
-      <Text>
-        🙋 You have questions? Ask them on the{' '}
-        <Link href={links.forum} isExternal alignItems={'center'}>
-          Moralis forum <ExternalLinkIcon />
-        </Link>
-      </Text>
-      <Text>
-        📖 Read more about{' '}
-        <Link href={links.moralis} isExternal alignItems={'center'}>
-          Moralis <ExternalLinkIcon />
-        </Link>
-      </Text>
+      {/*<Text>*/}
+      {/*  ⭐️ Please star this{' '}*/}
+      {/*  <Link href={links.github} isExternal alignItems={'center'}>*/}
+      {/*    boilerplate <ExternalLinkIcon />*/}
+      {/*  </Link>*/}
+      {/*  , every star makes us very happy!*/}
+      {/*</Text>*/}
+      {/*<Text>*/}
+      {/*  🙋 You have questions? Ask them on the{' '}*/}
+      {/*  <Link href={links.forum} isExternal alignItems={'center'}>*/}
+      {/*    Moralis forum <ExternalLinkIcon />*/}
+      {/*  </Link>*/}
+      {/*</Text>*/}
+      {/*<Text>*/}
+      {/*  📖 Read more about{' '}*/}
+      {/*  <Link href={links.moralis} isExternal alignItems={'center'}>*/}
+      {/*    Moralis <ExternalLinkIcon />*/}
+      {/*  </Link>*/}
+      {/*</Text>*/}
     </Box>
   );
 };
--- src/components/templates/home/Home.tsx
+++ src/components/templates/home/Home.tsx
@@ -1,62 +1,124 @@
-import { CheckCircleIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Heading, VStack, List, ListIcon, ListItem } from '@chakra-ui/react';
+import {Container, Heading, NumberInput, NumberInputField, useControllableState, VStack} from '@chakra-ui/react';
+import {Badge, OptionProps, Select} from "@web3uikit/core";
+import {Fetcher, Pair, Trade} from '@reservoir-labs/sdk'
+import {CurrencyAmount, Token, TradeType} from "@reservoir-labs/sdk-core";
+import {BaseProvider, WebSocketProvider} from "@ethersproject/providers";
+import {useEffect} from "react";
+
+const tokenSelectOptions: OptionProps[] = [{label: 'USDC', id: 'USDC'}, {label:'WAVAX', id: 'WAVAX'}, {label: 'USDT', id: 'USDT'}]
+
+const CHAINID = 43114
+
+const TOKEN_ADDRESS = {
+    43114: {
+        'USDC': '0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5',
+        'USDT': '0xfbC22278A96299D91d41C453234d97b4F5Eb9B2d',
+        'WAVAX': '0x46b142DD1E924FAb83eCc3c08e4D46E82f005e0E'
+    }
+}
 
 const Home = () => {
+  const [fromToken, setFromToken] = useControllableState({defaultValue: null})
+  const [toToken, setToToken] = useControllableState({defaultValue: null})
+  const [fromAmount, setFromAmount] = useControllableState({defaultValue: ''})
+  const [toAmount, setToAmount] = useControllableState({defaultValue: ''})
+  const [swapType, setSwapType] = useControllableState({defaultValue: null})
+  const provider: BaseProvider = new WebSocketProvider('ws://127.0.0.1:8545')
+
+  const _handleQuoteChange = async () => {
+
+    console.log(fromToken)
+    console.log(toToken)
+    console.log(fromAmount)
+    console.log(toAmount)
+    if (fromToken == null || toToken == null) {
+        return
+    }
+    if (fromToken.id === toToken.id) {
+        return
+    }
+    if (fromAmount === '' && swapType === TradeType.EXACT_INPUT) {
+        return
+    }
+
+    const from = new Token(CHAINID, TOKEN_ADDRESS[CHAINID][fromToken.id], 18)
+    const to = new Token(CHAINID, TOKEN_ADDRESS[CHAINID][toToken.id], 18)
+
+    const relevantPairs: Pair[] = await Fetcher.fetchRelevantPairs(
+      CHAINID,
+      from,
+      to,
+      provider
+    )
+
+    console.log("relevant", relevantPairs)
+
+    if (swapType === TradeType.EXACT_INPUT) {
+        const route: Trade<Token, Token, TradeType.EXACT_INPUT>[] = Trade.bestTradeExactIn(
+            relevantPairs,
+            CurrencyAmount.fromRawAmount(from, fromAmount),
+            to,
+            { maxNumResults: 3, maxHops: 2},
+        )
+
+        if (route.length > 0) {
+            setToAmount(route[0].outputAmount.toExact())
+        }
+    }
+    else if (swapType === TradeType.EXACT_OUTPUT) {
+        const route: Trade<Token, Token, TradeType.EXACT_OUTPUT>[] = Trade.bestTradeExactOut(
+            relevantPairs,
+            from,
+            CurrencyAmount.fromRawAmount(to, toAmount),
+            { maxNumResults: 3, maxHops: 2},
+        )
+
+        if (route.length > 0) {
+            setFromAmount(route[0].inputAmount.toExact())
+        }
+    }
+  }
+
+  const fromTokenChanged = (option: OptionProps) => {
+      setFromToken(option)
+  }
+
+  const toTokenChanged = (option: OptionProps) => {
+    setToToken(option)
+  }
+
+  const fromAmountChanged = (valString: string, valNum: number) => {
+    setSwapType(TradeType.EXACT_INPUT)
+    setFromAmount(valNum)
+  }
+
+  const toAmountChange = (valString: string, valNum: number) => {
+    setSwapType(TradeType.EXACT_OUTPUT)
+    setToAmount(valNum)
+  }
+
+
+  useEffect(() => {
+      _handleQuoteChange()
+  }, [fromToken, toToken, fromAmount, toAmount, swapType])
+
   return (
     <VStack w={'full'}>
       <Heading size="md" marginBottom={6}>
-        Ethereum Boilerplate
+        Sample Swap
       </Heading>
-      <List spacing={3}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Moralis authentication
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display Transactions
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display ERC20 balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT balances
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Display NFT transfers
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Multichain Support
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          Using Moralis from client-side
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Adding explorer links to balances, transactions ...
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Better responsive design
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          Rainbowkit integration
-        </ListItem>
-        <ListItem>
-          <ListIcon as={SettingsIcon} color="green.500" />
-          ... and more
-        </ListItem>
-      </List>
+      <Container>
+        <Badge text={'From Token'}/>
+          <Select label='select a token' id={'from'} options={tokenSelectOptions} onChange={fromTokenChanged}/>
+          <NumberInput min={0} id='input-amount' value={fromAmount} onChange={fromAmountChanged}>
+              <NumberInputField />
+          </NumberInput>
+        <Badge text={'To Token'}/>
+          <Select label='select a token' id={'to'} options={tokenSelectOptions} onChange={toTokenChanged}/>
+          <NumberInput min={0} id='output-amount' value={toAmount} onChange={toAmountChange}>
+              <NumberInputField />
+          </NumberInput>
+      </Container>
     </VStack>
   );
 };
